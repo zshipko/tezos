ce0eb57873b8737d28e98791894ed11c712b0fb5
icristescu <domninac@gmail.com>
2020-08-31 12:59:57 +0200

    Layered store

--- src/lib_context/context.ml
+++ src/lib_context/context.ml
@@ -289,7 +289,7 @@ module Conf = struct
 end
 
 module Store =
-  Irmin_pack.Make_ext (Conf) (Irmin.Metadata.None) (Contents)
+  Irmin_pack.Make_ext_layered (Conf) (Irmin.Metadata.None) (Contents)
     (Irmin.Path.String_list)
     (Irmin.Branch.String)
     (Hash)
@@ -371,7 +371,12 @@ let unshallow context =
               >|= fun _ -> ())
         children)
 
+let counter = ref 0
+
+let first = ref true
+
 let raw_commit ~time ?(message = "") context =
+  counter := succ !counter ;
   let info =
     Irmin.Info.v ~date:(Time.Protocol.to_seconds time) ~author:"Tezos" message
   in
@@ -379,7 +384,17 @@ let raw_commit ~time ?(message = "") context =
   unshallow context
   >>= fun () ->
   Store.Commit.v context.index.repo ~info ~parents context.tree
-  >|= fun h ->
+  >>= fun h ->
+  ( if !first then (
+    first := false ;
+    Store.freeze ~max:[h] context.index.repo )
+  else Lwt.return_unit )
+  >>= fun () ->
+  ( if !counter = 4000 then (
+    counter := 0 ;
+    Store.freeze ~max:[h] context.index.repo )
+  else Lwt.return_unit )
+  >|= fun () ->
   Store.Tree.clear context.tree ;
   h
 
@@ -507,16 +522,21 @@ let fork_test_chain v ~protocol ~expiration =
 
 (*-- Initialisation ----------------------------------------------------------*)
 
-let init ?patch_context ?(readonly = false) root =
+let config ?readonly root =
   let index_throttle =
     if !overcommit then `Overcommit_memory else `Block_writes
   in
-  Store.Repo.v
-    (Irmin_pack.config
-       ~readonly
-       ?index_log_size:!index_log_size
-       ~index_throttle
-       root)
+  let conf =
+    Irmin_pack.config
+      ?readonly
+      ?index_log_size:!index_log_size
+      ~index_throttle
+      root
+  in
+  Irmin_pack.config_layers ~conf ~copy_in_upper:true ~with_lower:true ()
+
+let init ?patch_context ?(readonly = false) root =
+  Store.Repo.v (config ~readonly root)
   >>= fun repo ->
   let v = {path = root; repo; patch_context; readonly} in
   Lwt.return v
